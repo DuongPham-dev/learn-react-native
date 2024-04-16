fa18c9b4e16b8735424083024f6f25982bef205f
Duong Pham <phamtanduongtk29@gmail.com>
2024-04-16 15:45:35 +0700

    comapre ScrollView vs FlatList

--- README.md
+++ README.md
@@ -1,16 +1,25 @@
 # React Native
 
 ## What is the React Native?
- - React Native is an open-source framework for building Android and iOS applications using React and the app platform’s native capabilities.
-  
+
+- React Native is an open-source framework for building Android and iOS applications using React and the app platform’s native capabilities.
+
 ## Difference between Expo CLI and React Native CLI
+
 ### Expo CLI
- - Is the tool to help you create React Native projects and run React Native apps and run React Native apps on simulated devices or testing devices. 
- - It had the services that were free.
- - It managed app development (creating project so easily, and code so easily, ...).
- - Writing native app by React Native will favorable than React Native CLI.
+
+- Is the tool to help you create React Native projects and run React Native apps and run React Native apps on simulated devices or testing devices.
+- It had the services that were free.
+- It managed app development (creating project so easily, and code so easily, ...).
+- Writing native app by React Native will favorable than React Native CLI.
+
 ### React Native CLI
-  - Is the tool to help you create React Native projects and run React Native apps and run React Native apps on simulated devices or testing devices. (It exist before Expo CLI).
-  - It provided from React Native team and community around it.
-  - It provide a setup develop React Native basic (meaning you should add more configs or setup, it less convennience when use some feature for native device as Camera and ...).
-  -  Easier integration with native source code.
+
+- Is the tool to help you create React Native projects and run React Native apps and run React Native apps on simulated devices or testing devices. (It exist before Expo CLI).
+- It provided from React Native team and community around it.
+- It provide a setup develop React Native basic (meaning you should add more configs or setup, it less convennience when use some feature for native device as Camera and ...).
+- Easier integration with native source code.
+
+## Difference between ScrollView and FlatList
+
+- ScrollView and FlatList are both used to facilitate scrolling when items exceed the viewport. However, the key difference is that when rendering many items (more than 100 items), ScrollView may display all items at once, while FlatList will display a portion and lazily load the remaining items as you scroll down. As a result, you should use ScrollView for user interfaces with a fixed amount of scrolling, and FlatList for dynamic lists.
--- expo-cli-pnpm/App.js
+++ expo-cli-pnpm/App.js
@@ -1,23 +1,115 @@
-import { StyleSheet, Text, View } from "react-native";
+import { useCallback, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Pressable,
+  FlatList,
+  ScrollView,
+} from "react-native";
 
 export default function App() {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState(
+    Array.from({ length: 1 }).map((_, index) => ({
+      id: index,
+      name: "112323",
+    }))
+  );
+
+  const handleChangeTask = useCallback((value) => {
+    setTask(value);
+  }, []);
+
+  const handleAddTask = () => {
+    setTasks((prev) => {
+      return [
+        {
+          id: prev[prev.length - 1].id + 1,
+          name: task,
+        },
+        ...prev,
+      ];
+    });
+
+    setTask("");
+  };
+
   return (
-    <Text
-      style={{
-        color: "red",
-        fontSize: 20,
-      }}
-    >
-      Open up App.js to start working on your app!
-    </Text>
+    <View style={style.container}>
+      {/* Information entry section */}
+      <View style={style.addTaskBox}>
+        <TextInput
+          placeholder="Enter task name..."
+          style={style.enterText}
+          value={task}
+          onChangeText={handleChangeTask}
+        />
+        <Pressable style={style.addButton} onPress={handleAddTask}>
+          <Text style={{ color: "white", fontSize: 15 }}>Add task</Text>
+        </Pressable>
+      </View>
+
+      {/* Display tasks */}
+      <FlatList
+        data={tasks}
+        // keyExtractor={(i) => i.id}
+        renderItem={(task) => {
+          console.log("task.index", task.index);
+
+          return (
+            <Text key={task.index} style={style.task}>
+              {task.item.name}
+            </Text>
+          );
+        }}
+        ItemSeparatorComponent={() => <View style={{ height: 16 }} />}
+      />
+      {/* <ScrollView>
+        {tasks.map((task, index) => {
+          console.log("index", index);
+
+          return (
+            <Text key={index} style={style.task}>
+              {task.name}
+            </Text>
+          );
+        })}
+      </ScrollView> */}
+    </View>
   );
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   container: {
+    padding: 20,
+  },
+  addTaskBox: {
+    paddingVertical: 20,
+    display: "flex",
+    flexDirection: "row",
+    gap: 10,
+  },
+  enterText: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 10,
+    paddingLeft: 20,
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "green",
+  },
+  addButton: {
+    backgroundColor: "blue",
+    paddingHorizontal: 25,
+    paddingVertical: 15,
+    borderRadius: 10,
+  },
+  task: {
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 10,
+    fontSize: 16,
+    fontWeight: "600",
   },
 });
